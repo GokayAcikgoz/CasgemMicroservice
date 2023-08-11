91cfa66ad3a1a37298e042639698c0ead0c4f3b4
gokay <gokayacikgoz1@gmail.com>
2023-08-11 16:42:37 +0300

    onion arc

--- Services/Order/Core/CasgemMicroservice.Services.Order.Core.Application/Dtos/OrderDetailDtos/CreateOrderDetailDto.cs
+++ Services/Order/Core/CasgemMicroservice.Services.Order.Core.Application/Dtos/OrderDetailDtos/CreateOrderDetailDto.cs
@@ -4,16 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CasgemMicroservice.Services.Order.Core.Domain.Entities
+namespace CasgemMicroservice.Services.Order.Core.Application.Dtos.OrderDetailDtos
 {
-    public class OrderDetail
+    public class CreateOrderDetailDto
     {
-        public int OrderDetailID { get; set; }
         public string ProductID { get; set; }
         public string ProductName { get; set; }
         public decimal ProductPrice { get; set; }
         public int ProductAmount { get; set; }
         public int OrderingID { get; set; }
-        public Ordering Ordering { get; set; }
     }
 }
