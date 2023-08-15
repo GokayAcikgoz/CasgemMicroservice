c7d6773c1b931cbf3474fc9dad07dadcc898a283
gokay <gokayacikgoz1@gmail.com>
2023-08-15 11:43:11 +0300

    payment microservice and cargo microservice

--- IdentityServer/CasgemMicroservice.IdentityServer/Config.cs
+++ IdentityServer/CasgemMicroservice.IdentityServer/Config.cs
@@ -17,7 +17,8 @@ namespace CasgemMicroservice.IdentityServer
                     new ApiResource("resource_basket"){Scopes = {"basket_fullpermission"}},                   
                         new ApiResource("resource_discount"){Scopes = {"discount_fullpermission"}},                   
                             new ApiResource("resource_order"){Scopes = {"order_fullpermission"}},                   
-                                new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
+                                new ApiResource("resource_cargo"){Scopes = {"cargo_fullpermission"}},                   
+                                    new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
         };
         public static IEnumerable<IdentityResource> IdentityResources =>
                    new IdentityResource[]
@@ -35,7 +36,8 @@ namespace CasgemMicroservice.IdentityServer
                         new ApiScope("basket_fullpermission", "Sepet İşlemleri İçin Tam Erişim"),
                             new ApiScope("discount_fullpermission", "İndirim İşlemleri İçin Tam Erişim"),
                                 new ApiScope("order_fullpermission", "Sipariş İşlemleri İçin Tam Erişim"),
-                                    new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
+                                    new ApiScope("cargo_fullpermission", "Sipariş İşlemleri İçin Tam Erişim"),
+                                        new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
             };
 
         public static IEnumerable<Client> Clients =>
@@ -62,7 +64,7 @@ namespace CasgemMicroservice.IdentityServer
                     AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
 
                     AllowOfflineAccess = true,
-                    AllowedScopes = { "catalog_fullpermission", "basket_fullpermission", "photoStock_fullpermission", "discount_fullpermission", "order_fullpermission",
+                    AllowedScopes = { "catalog_fullpermission", "basket_fullpermission", "photoStock_fullpermission", "discount_fullpermission", "order_fullpermission", "cargo_fullpermission",
                         IdentityServerConstants.LocalApi.ScopeName, 
                         IdentityServerConstants.StandardScopes.Email,
                         IdentityServerConstants.StandardScopes.OpenId,
--- Services/Cargo/CasgemMicroservice.Services.Cargo.WebApi/Program.cs
+++ Services/Cargo/CasgemMicroservice.Services.Cargo.WebApi/Program.cs
@@ -1,11 +1,42 @@
+using CasgemMicroservice.Services.Cargo.BusinessLayer.Abstract;
+using CasgemMicroservice.Services.Cargo.BusinessLayer.Concrete;
+using CasgemMicroservice.Services.Cargo.DataAccessLayer.Abstract;
+using CasgemMicroservice.Services.Cargo.DataAccessLayer.Context;
+using CasgemMicroservice.Services.Cargo.DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using System.IdentityModel.Tokens.Jwt;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+
+var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+{
+    opt.Authority = builder.Configuration["IdentityServerUrl"];
+    opt.Audience = "resource_cargo";
+    opt.RequireHttpsMetadata = false;
+});
+
+builder.Services.AddControllers(opt =>
+{
+    opt.Filters.Add(new AuthorizeFilter(requireAuthorizePolicy)); //kullanýcýyý giriþþe zorladýk
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<CargoContext>();
+builder.Services.AddScoped<ICargoDetailService, CargoDetailManager>();
+builder.Services.AddScoped<ICargoDetailDal, EfCargoDetailDal>();
+builder.Services.AddScoped<ICargoStateService, CargoStateManager>();
+builder.Services.AddScoped<ICargoStateDal, EfCargoStateDal>();
 
 var app = builder.Build();
 
@@ -18,6 +49,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
